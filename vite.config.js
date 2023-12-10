a066e5d83a309330a6f14ee401fdc675540503f9
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-12-10 14:57:23 +0530

    as

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [react()],
+  base: '/',
+  server: {
+    proxy: {
+      '/all-jobs': {
+        target: 'https://mern-job-api.vercel.app/',
+        // target:'http://localhost:5000',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
 })
