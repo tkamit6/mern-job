24ab916007219475262aceccff2485c7b0a883d0
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-12-15 11:38:11 +0530

    v

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -38,7 +38,7 @@ export default function Login() {
     return (
         <div className='h-screen w-full items-center justify-center flex'>
             {
-                useName ? `Welcome ${useName}` : <Button variant='contained' onClick={handleLogin} className='!py-2 !px-8 !bg-blue !text-white !gap-4' > {useName} <FcGoogle /> Login</Button>
+                useName ? `Welcome ${useName}` : <Button variant='outlined' onClick={handleLogin} className='!py-2 !px-8 !text-white !gap-4' > {useName} <FcGoogle /> Login</Button>
             }
 
         </div>
